e8ffc5a850d41270c141f8f9eb4b3c64e6778255
Max Rydahl Andersen <max@jboss.org>
2013-06-03 11:09:18 +0200

    cleanup team list

--- jbdevstudio-teams/addmembers.sh
+++ jbdevstudio-teams/addmembers.sh
@@ -1,16 +1,38 @@
 
-team=$1
+for name in "$@"
+do
 
-teamid=`curl -u $GITHUB_USER:$GITHUB_PWD https://api.github.com/orgs/jbdevstudio/teams | grep "name...*$team" -A1  | grep id | sed -e 's/.*\"id...\(.*\),/\1/g'`
+echo ------ Trying to setup permissions for $name
+
+file=$(basename $name)
+team="${file%.*}"
+
+teamid=`curl -# -u $GITHUB_USER:$GITHUB_PWD https://api.github.com/orgs/jbdevstudio/teams | grep "name...*$team" -A1  | grep id | sed -e 's/.*\"id...\(.*\),/\1/g'`
 
 echo Found $team having $teamid as id
 
-cat $1.team | xargs -n 1 -I {} bash -c "echo Adding {} to $team && curl -u $GITHUB_USER:$GITHUB_PWD -X PUT -d ''  https://api.github.com/teams/$teamid/members/{}"
+cat $team.team | xargs -n 1 -I {} bash -c "echo Adding {} to $team && curl -u $GITHUB_USER:$GITHUB_PWD -X PUT -d ''  https://api.github.com/teams/$teamid/members/{}"
 
-echo Add repo with same name as team to this repo
-curl -u $GITHUB_USER:$GITHUB_PWD -X PUT -d '' https://api.github.com/teams/$teamid/repos/jbdevstudio/$team
+if [[ $team == jbdevstudio-* ]] ;
+then 
+    echo Adding repo $team to team $team
+    curl -# -u $GITHUB_USER:$GITHUB_PWD -X PUT -d "" https://api.github.com/teams/$teamid/repos/jbdevstudio/$team
+fi
 
+curl -# -u $GITHUB_USER:$GITHUB_PWD https://api.github.com/teams/$teamid/members | grep \"login\"\: | sed -e 's/.*login":.*"\(.*\)",/\1/g' | sort > $team.existingmembers
 
+# ensure hand written team is actually sorted.
+sort $team.team -o $team.team
+
+echo Comparing...
+diff=`diff $team.existingmembers $team.team`
+if [ "$diff" != "" ]
+then
+   echo WARNING: Team $team does not have the exact members expected! Please check.
+   echo "$diff"
+fi
+
+done
 
 
 
